62739458ad573817f9348353f3b34c0d26c9c30f
Sindre Sorhus <sindresorhus@gmail.com>
2019-07-29 21:57:25 +0200

    Update api.js

--- api.js
+++ api.js
@@ -26,7 +26,7 @@ module.exports = async (filePaths, {find, replacement, ignoreCase} = {}) => {
 		throw new Error('The `replacement` option is required');
 	}
 
-	// Replace replacement string with string unescaped (only one backslash) if it is escaped
+	// Replace the replacement string with the string unescaped (only one backslash) if it's escaped
 	replacement = replacement
 		.replace(/\\n/g, '\n')
 		.replace(/\\r/g, '\r')
