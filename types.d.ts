317548a909d52fd8ebbbbac8be62ab3e8766322a
Sindre Sorhus <sindresorhus@gmail.com>
2023-05-14 14:14:36 +0700

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,9 @@
-declare module 'replace-in-files-cli' {
-  export default function replaceInFiles(
-    path: string | string[],
-    options: {
-      find: (string | RegExp)[]
-      replacement: string
-      ignoreCase?: boolean
-      glob?: boolean
-    },
-  ): Promise<void>
-}
+export default function replaceInFiles(
+	path: string | string[],
+	options: {
+		find: Array<string | RegExp>,
+		replacement: string,
+		ignoreCase?: boolean,
+		glob?: boolean
+	},
+): Promise<void>;
